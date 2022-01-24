b000aab1a35ca637883b5ba60b141835c8ec5992
weedman-2000 <77209462+weedman-2000@users.noreply.github.com>
2022-01-23 23:16:56 -0500

    cambios para el toolbar

--- app/src/main/java/com/fmb/icookproyect_/FoodEntryFragment.java
+++ app/src/main/java/com/fmb/icookproyect_/FoodEntryFragment.java
@@ -2,20 +2,52 @@ package com.fmb.icookproyect_;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 public class FoodEntryFragment extends Fragment
 {
+    @Override
+    public void onCreate(Bundle savedInstanceSate)
+    {
+        super.onCreate(savedInstanceSate);
+        setHasOptionsMenu(true);
+    }
+
     @Override
     public View onCreateView
             (
                 @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState
             )
     {
-        return inflater.inflate(R.layout.food_entry_fragment, container, false);
+        //return inflater.inflate(R.layout.food_entry_fragment, container, false);
+        View view = inflater.inflate(R.layout.food_entry_fragment, container, false);
+
+        setUpToolBar(view);
+        return view;
+    }
+
+    private void setUpToolBar(View view)
+    {
+        Toolbar toolbar = view  .findViewById(R.id.app_bar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+
+        if(activity != null)
+        {
+            activity.setSupportActionBar(toolbar);
+        }
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater)
+    {
+        menuInflater.inflate(R.menu.toolbar_menu, menu);
+        super.onCreateOptionsMenu(menu, menuInflater);
     }
 }
