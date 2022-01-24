a7c5313d98a66f3184b4fe1cd3548996289f4d3a
weedman-2000 <77209462+weedman-2000@users.noreply.github.com>
2022-01-24 01:42:41 -0500

    clase2 Semana 8
    min12:14

---
+++ app/src/main/java/com/fmb/icookproyect_/network/FoodEntry.java
@@ -0,0 +1,11 @@
+package com.fmb.icookproyect_.network;
+
+public class FoodEntry
+{
+    public final String title;
+    public final Uri    dynamicUrl;
+    public final String Url;
+    public final String type;
+    
+
+}
